4876ef2a17d87a5ea0231af575850be317ad1685
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-08-31 16:52:38 +0700

    Update function register

--- routes/authentication.routes.js
+++ routes/authentication.routes.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+
+//Use sequelize model
+const db = require('../config/database.config');
+const User = db.user;
 
 //Login
 router.get('/login', (req, res) => {
@@ -7,8 +12,37 @@ router.get('/login', (req, res) => {
 
 
 //Register
-router.get('/register', (req, res) => {
-  res.status(200).send('Register');
-})
+router.post('/register', async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
+
+  //check null information
+  if (!(email && password && first_name && last_name)) return res.status(400).json({ "message": "information is required" })
+
+  //check user in db
+  const user = await User.findOne({ where: { email: email }});
+
+  if (user) return res.status(400).json({
+    "message": "user already exists"
+  });
+
+  //hash password
+  const hashPassword = await bcrypt.hash(password, 8);
+
+  //create new user
+  await User.create({
+    email: email,
+    password: hashPassword,
+    first_name: first_name,
+    last_name: last_name,
+  });
+
+  //response
+  res.status(200).json({
+    "message": "register successfully",
+  });
+});
 
 module.exports = router;
