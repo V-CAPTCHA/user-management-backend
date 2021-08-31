e87a111bb58b122433a61122567fb2aef212a14d
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-08-31 12:30:56 +0700

    Init authen action model

---
+++ models/authen_action.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, Sequelize) => {
+  const AuthenAction = Sequelize.define(
+    //Table name
+    'authen_action', 
+    {
+      //Fields
+    }
+  )
+
+  return AuthenAction;
+};
