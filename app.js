e2642dc21c2697c11577071e7354a6be9e2e89bb
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-10 16:19:11 +0700

    Add auth middleware to routes

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ const users = require('./routes/users.routes');
 const keys = require('./routes/keys.routes');
 const dashboard = require('./routes/dashboard.routes');
 
+//middleware
+const verifyToken = require('./middleware/authentication.middleware');
+
 //Create app
 const app = express();
 
 //Router
 app.use('/api', authentication);
-app.use('/api/users', users);
-app.use('/api/keys', keys);
-app.use('/api/dashboard', dashboard);
+app.use('/api/users', verifyToken, users);
+app.use('/api/keys', verifyToken, keys);
+app.use('/api/dashboard', verifyToken, dashboard);
 
 
 module.exports = app;
