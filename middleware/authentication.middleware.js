7a71cceb5eb668786e1a876bb69e0584c0f1ae39
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-10 15:54:17 +0700

    Update verifyToken function

--- middleware/authentication.middleware.js
+++ middleware/authentication.middleware.js
@@ -1,7 +1,28 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
+  //Get token
+  const token = req.body.token || req.headers['x-access-token'] || req.headers['x-token'];
 
+  //If no token
+  if (!token) {
+    return res.status(403).json({
+      'message': 'token is required for authentication'
+    });
+  }
+
+  try {
+    //add info to res.locals
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    res.locals.user = decoded;
+  }
+  catch(error) {
+    return res.status(401).json({
+      'message': 'Invalid token'
+    });
+  }
+
+  //next function after token checked
   return next();
 }
 
