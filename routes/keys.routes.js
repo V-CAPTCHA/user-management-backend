99e94e75adb08fba2864fc3cf1c27da4d9ce3d2c
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-15 20:00:41 +0700

    Update function get all keys

--- routes/keys.routes.js
+++ routes/keys.routes.js
@@ -1,8 +1,31 @@
 const router = require('express').Router();
 
+//Use sequelize model
+const db = require('../config/database.config');
+const CaptchaKey = db.captcha_key;
+
 //Get all keys
-router.get('/', (req, res) => {
-  res.status(200).send('Keys');
+router.get('/', async (req, res) => {
+  const user_id = res.locals.user.user_id;
+
+  //find all key of user in db
+  const keys = await CaptchaKey.findAll({
+    attributes: [
+      'key_id',
+      'key_name',
+      'creation_date',
+      'domain',
+      'key_value'
+    ],
+    where: {
+      user_id: user_id
+    }
+  })
+
+  res.status(200).json({
+    "message": "get all keys successfully",
+    "data": keys
+  })
 })
 
 module.exports = router;
