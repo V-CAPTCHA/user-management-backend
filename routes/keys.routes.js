cfb7fe32cd03f65191475d72829cef3b82470d46
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-15 20:40:09 +0700

    Add function get key info

--- routes/keys.routes.js
+++ routes/keys.routes.js
@@ -26,12 +26,53 @@ router.get('/', async (req, res) => {
   if(!count) {
     return res.status(200).json({"message": "key does not exist"});
   }
-  else {
-    res.status(200).json({
-      "message": "get all keys successfully",
-      "data": rows
-    })
-  }
+  
+  //response
+  res.status(200).json({
+    "message": "get all keys successfully",
+    "data": rows
+  })
 });
+
+
+//Get key info
+router.get('/:key_value', async (req, res) => {
+  const key_value = req.params.key_value;
+  const user_id = res.locals.user.user_id;
+  
+  //check null params
+  if(!key_value) {
+    return res.status(400).json({"message": "key values is requried"})
+  }
+
+  //find key in db
+  const key = await CaptchaKey.findOne({
+    attributes: [
+      'key_id',
+      'key_name',
+      'creation_date',
+      'domain',
+      'key_value'
+    ],
+    where: { 
+      user_id: user_id,
+      key_value: key_value 
+    }
+  });
+
+  //if key does not exist
+  if(!key) {
+    return res.status(400).json({"message": "key does not exist"});
+  }
+
+  //response
+  res.status(200).json({
+    "message": "get key successfully",
+    "data": key
+  });
+});
+
+
+
 
 module.exports = router;
