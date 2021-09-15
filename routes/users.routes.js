7494c2252b77a47155b2a9b5a77b4623c2eed4eb
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-15 16:42:33 +0700

    Add function change first name and last name

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -26,7 +26,42 @@ router.get('/', async (req, res) => {
   res.status(200).json({
     "message": "get user successfully",
     "data": user
-  })
+  });
 })
 
 module.exports = router;
+
+
+//Change first name and last name
+router.post('/', async (req, res) => {
+  const user_id = res.locals.user.user_id;
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
+
+  //check null information
+  if (!first_name || !last_name) {
+    return res.status(401).json({"message": "information is required"});
+  }
+
+  //find user in db
+  const user = await User.findOne({ 
+    where: { user_id: user_id }
+  });
+
+  if (!user)
+    return res.status(400).json({"message": "user does not exist "});
+
+  //update user
+  await User.update(
+    {  
+      first_name: first_name,
+      last_name: last_name,
+    },
+    { where: { user_id: user_id }}
+  );
+
+  //response
+  res.status(200).json({
+    "message": "change first name and last name successfully"
+  })
+});
