46fc3695f7f45d11147d3e9f9bc414dea1a77c69
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-15 16:27:06 +0700

    Update function get user info

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,32 @@
 const router = require('express').Router();
 
-//Get all users
-router.get('/', (req, res) => {
-  res.status(200).send('Users');
+//Use sequelize model
+const db = require('../config/database.config');
+const User = db.user;
+
+
+//Get a user
+router.get('/', async (req, res) => {
+  const user_id = res.locals.user.user_id;
+  
+  //find user in db
+  const user = await User.findOne({ 
+    attributes: [
+      'email',
+      'first_name',
+      'last_name',
+    ],
+    where: { user_id: user_id }
+  });
+
+  if (!user)
+    return res.status(400).json({"message": "user does not exist "});
+
+  //response
+  res.status(200).json({
+    "message": "get user successfully",
+    "data": user
+  })
 })
 
 module.exports = router;
