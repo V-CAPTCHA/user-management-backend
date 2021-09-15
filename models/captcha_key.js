7c83f8f39586b4531227454ed34d9e53b34cf307
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-15 20:00:17 +0700

    Update captcha_key model

--- models/captcha_key.js
+++ models/captcha_key.js
@@ -1,11 +1,43 @@
 module.exports = (sequelize, Sequelize) => {
   const CaptchaKey = sequelize.define(
     //Table name
-    'captcha_key', 
+    'captcha_keys', 
     {
       //Fields
+      key_id: {
+        type: Sequelize.INTEGER,
+        field: 'key_id',
+        primaryKey: true,
+        authoIncrement: true,
+      },
+      key_name: {
+        type: Sequelize.STRING,
+        field: 'key_name',
+      },
+      creation_date: {
+        type: Sequelize.DATE,
+        field: 'creation_date',
+      },
+      domain: {
+        type: Sequelize.STRING,
+        field: 'domain',
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        field: 'user_id',
+        foreignKey: true,
+      },
+      key_value: {
+        type: Sequelize.STRING,
+        field: 'key_value',
+      },
+    },
+    //options
+    {
+      timestamps: false,
+      freezeTableName: true,
     }
-  )
+  );
 
   return CaptchaKey;
 };
