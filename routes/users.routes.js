c1a98d96231fe1ee93af31eb2eeded52f69d5ee2
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-15 19:23:01 +0700

    Add function delete account

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 
 //Use sequelize model
 const db = require('../config/database.config');
 const User = db.user;
+const CaptchaKey = db.captcha_key;
 
 
 //Get a user
@@ -40,7 +42,7 @@ router.post('/', async (req, res) => {
 
   //check null information
   if (!first_name || !last_name) {
-    return res.status(401).json({"message": "information is required"});
+    return res.status(400).json({"message": "information is required"});
   }
 
   //find user in db
@@ -64,4 +66,37 @@ router.post('/', async (req, res) => {
   res.status(200).json({
     "message": "change first name and last name successfully"
   })
+});
+
+
+//Delete account
+router.delete('/', async (req, res) => {
+  const user_id = res.locals.user.user_id;
+
+  //find user in db
+  const user = await User.findOne({ 
+    where: { user_id: user_id }
+  });
+
+  if (!user)
+    return res.status(400).json({"message": "user does not exist "});
+
+  //delete captcha_key of user
+  await CaptchaKey.destroy({
+    where: {
+      user_id: user_id
+    }
+  });
+
+  //delete user
+  await User.destroy({
+    where: {
+      user_id: user_id
+    }
+  });
+
+  //response
+  res.status(200).json({
+    "message": "delete account successfully"
+  });
 });
