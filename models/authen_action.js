2d2608f67d3cc20a3a3fea370079bd5336219d44
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-17 21:30:03 +0700

    Update model of authen_action table

--- models/authen_action.js
+++ models/authen_action.js
@@ -4,8 +4,51 @@ module.exports = (sequelize, Sequelize) => {
     'authen_action', 
     {
       //Fields
+      action_id: {
+        type: Sequelize.INTEGER,
+        field: 'action_id',
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      dataset_id: {
+        type: Sequelize.INTEGER,
+        field: 'dataset_id',
+      },
+      action_reply: {
+        type: Sequelize.STRING,
+        field: 'action_reply',
+      },
+      action_create: {
+        type: Sequelize.DATE,
+        field: 'action_create',
+      },
+      action_end: {
+        type: Sequelize.DATE,
+        field: 'action_end',
+      },
+      action_checked: {
+        type: Sequelize.BOOLEAN,
+        field: 'action_checked',
+      },
+      action_valid: {
+        type: Sequelize.STRING,
+        field: 'action_valid',
+      },
+      action_ip: {
+        type: Sequelize.STRING,
+        field: 'action_ip',
+      },
+      key_value: {
+        type: Sequelize.STRING,
+        field: 'key_value',
+        foreignKey: true,
+      }
+    },
+    {
+      timestamps: false,
+      freezeTableName: true,
     }
-  )
+  );
 
   return AuthenAction;
 };
