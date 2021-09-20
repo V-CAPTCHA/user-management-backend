a4579424b8edd28c48b4aad72e6a5ca2bdba4fb9
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-20 22:14:52 +0700

    Update function get valid-request

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const moment = require('moment');
 const sequelize = require('sequelize');
-const { Op, where } = require('sequelize')
+const { Op } = require('sequelize')
 
 //Use sequelize model
 const db = require('../config/database.config');
@@ -65,14 +65,72 @@ router.get('/total-request', async (req, res) => {
 
 //Valid request
 router.get('/valid-request', async (req, res) => {
+  const user_id = res.locals.user.user_id;
+
+  //define return data
+  var total_request = 0;
   var valid_percent = 0;
   var valid_request_per_day = [];
 
+  //define association CaptchaKey and AuthenAction
+  CaptchaKey.hasMany(AuthenAction, { foreignKey: 'key_value' });
+  AuthenAction.belongsTo(CaptchaKey, { foreignKey: 'key_value', targetKey: 'key_value' });
   
-  /*find valid action
-  const valid_action = await AuthenAction.findAndCountAll({
+  //total request of user
+  await AuthenAction.findAndCountAll({
+    include: {
+      model: CaptchaKey,
+      where: { user_id: user_id }
+    }
+  }).then((result) => {
+    total_request = result.count;
+  })
 
-  });*/
+  //valid request of user
+  await AuthenAction.findAndCountAll({
+    where: { action_checked: true },
+    include: {
+      model: CaptchaKey,
+      where: { user_id: user_id }
+    }
+  }).then((result) => {
+    //calculate percentage
+    valid_percent = (result.count / total_request)*100;
+    valid_percent = valid_percent.toFixed(2) + '%';
+  })
+
+  //find 90 day before action in db 
+  //atDate = 0 is current date
+  for(var atDate=-89; atDate<1; atDate++) {
+    var temp_date = moment().add(atDate, 'days').format('YYYY-MM-DD')
+
+    await AuthenAction.findAndCountAll({
+      where: {
+        [Op.and]: [
+          { action_checked: true },
+          sequelize.where(
+            sequelize.fn('date', sequelize.col('action_create')), '=', temp_date
+          ),
+        ]
+      },      
+      include: {
+        model: CaptchaKey,
+        where: {
+          user_id: user_id
+        }
+      }
+    })
+    .then((result) => {
+      valid_request_per_day.push(result.count);
+    })
+  }
+
+  //response
+  res.status(200).json({
+    'message':'get dashboard info successfully',
+    'valid_percent': valid_percent,
+    'valid_request_per_day': valid_request_per_day,
+  });
 });
 
 
