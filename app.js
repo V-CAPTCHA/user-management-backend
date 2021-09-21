231f5c037edc685349d0b243d41e2a5cda08dc77
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-22 03:10:58 +0700

    Add cors configuration

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require('express');
+const cors = require('cors')
 
 //import routes
 const authentication = require('./routes/authentication.routes');
@@ -13,7 +14,7 @@ const verifyToken = require('./middleware/authentication.middleware');
 //Create app
 const app = express();
 app.use(express.json());
-
+app.use(cors())
 
 //Router
 app.use('/api', authentication);
